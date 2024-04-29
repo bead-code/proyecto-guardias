bf769a5193d2cc55016a775460b1902fafa07fb1
Adolfo Montes Luque <montesluquead@gmail.com>
2024-04-29 14:35:18 +0200

    Añadir Footer y NavBar

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+
+export function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4">
+            <p>© 2024 Todos los derechos reservados</p>
+        </footer>
+    )
+}
