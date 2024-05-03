38ed9ce4b845a71c07e5b5c45863ab32d4485100
Adolofo Montes Luque <montesluquead@gmail.com>
2024-05-03 21:15:51 +0200

    Modificar proptypes

--- frontend/src/components/asignaturas/usuario/ListaAsignaturas.jsx
+++ frontend/src/components/asignaturas/usuario/ListaAsignaturas.jsx
@@ -17,22 +17,24 @@ export function ListaAsignaturas({asignaturas}) {
   );
 }
 ItemAsignatura.propTypes = {
+    asignatura: PropTypes.shape({
         id: PropTypes.number.isRequired,
         nombre: PropTypes.string.isRequired,
         habilitado: PropTypes.bool.isRequired,
         aula: PropTypes.string,
         curso: PropTypes.string.isRequired,
-        profesor: {
+        profesor: PropTypes.shape({
             nick: PropTypes.string.isRequired,
             color: PropTypes.string.isRequired,
             telefono: PropTypes.string.isRequired
-        },
+        }).isRequired,
         horas: PropTypes.arrayOf(
             PropTypes.shape({
                 dia: PropTypes.number.isRequired,
                 hora: PropTypes.number.isRequired
             })
         ).isRequired
+    }).isRequired
 }
 // Lista de asignaturas agrupadas por curso
 ListaAsignaturas.propTypes = PropTypes.arrayOf(
