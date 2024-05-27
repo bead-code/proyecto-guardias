50f68a1624d4f74c64bda6c8d519a26251c42295
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-05-27 10:19:29 +0200

    Modificar main adapart init para carga de datos

--- back/main.py
+++ back/main.py
@@ -1,21 +1,20 @@
-from datetime import datetime
-
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
-from db.database import Base, engine, Session
-from db.models import Rol, Profesor, Aula, Actividad, Curso, Horario
+from db.database import Base, engine, truncate_all_tables
 from generador_horarios.generador_actividades import load_actividades_from_xml
 from generador_horarios.generador_aulas import load_aulas_from_xml
-from generador_horarios.generador_profesores import load_profesores_from_xml
+from generador_horarios.generador_calendario import load_calendario_from_xml
+from generador_horarios.generador_clases import load_clases_from_xml
 from generador_horarios.generador_cursos import load_cursos_from_xml
-from routers import authentication, profesor, rol, ciclo, actividad, horario
-from security.hash import Hash
+from generador_horarios.generador_profesores import load_profesores_from_xml
+from generador_horarios.generador_roles import generar_roles
+from generador_horarios.generador_tramos_horarios import load_tramos_horarios_from_xml
+from routers import authentication, profesor, rol, curso, actividad, horario
 
 app = FastAPI()
 app.include_router(profesor.router)
 app.include_router(rol.router)
-app.include_router(ciclo.router)
+app.include_router(curso.router)
 app.include_router(actividad.router)
 app.include_router(authentication.router)
 app.include_router(horario.router)
@@ -23,6 +22,7 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://localhost:3000",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
@@ -36,17 +36,19 @@ app.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-def init_data(db: Session):
-        load_profesores_from_xml()
-        load_aulas_from_xml()
-        load_cursos_from_xml()
-        load_asignaturas_from_xml()
+def init_data():
+    generar_roles()
+    load_profesores_from_xml()
+    load_aulas_from_xml()
+    load_cursos_from_xml()
+    load_actividades_from_xml()
+    load_tramos_horarios_from_xml()
+    load_clases_from_xml()
+    load_calendario_from_xml()
 
 
 @app.on_event("startup")
 def on_startup():
-    db = Session()
-    try:
-        init_data(db)
-    finally:
-        db.close()
+    truncate_all_tables()
+    init_data()
+
