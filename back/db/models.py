95d5809a2f73df5bfc32cf9091ce58c16f687b50
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-05-27 10:18:49 +0200

    Modificar models adaptar xml

--- back/db/models.py
+++ back/db/models.py
@@ -5,13 +5,6 @@ from sqlalchemy import Column, String, Integer, Date, Enum as SQLAlchemyEnum, Bo
 from db.database import Base
 from security.hash import Hash
 
-class DiaSemanaEnum(PyEnum):
-    LUNES = 1
-    MARTES = 3
-    MIERCOLES = 3
-    JUEVES = 4
-    VIERNES = 5
-
 class Rol(Base):
     __tablename__ = 'roles'
     id_rol = Column(Integer, primary_key=True)
@@ -20,18 +13,18 @@ class Rol(Base):
 class Profesor(Base):
     __tablename__ = 'profesores'
     id_profesor = Column(Integer, primary_key=True)
-    username = Column(String(64), index=True, unique=True)
-    nombre = Column(String(64))
+    username = Column(String(64), unique=True)
     password = Column(String(255), nullable=False, default=Hash.argon2("Sampedro.1234"))
+    nombre = Column(String(64))
     password_temporal = Column(Boolean, nullable=False, default=True)
     color = Column(String(64))
-    id_rol= Column(Integer, ForeignKey('roles.codigo'))
+    id_rol= Column(Integer, ForeignKey('roles.id_rol'), nullable=False)
 
 
 class Curso(Base):
     __tablename__ = 'cursos'
     id_curso = Column(Integer, primary_key=True)
-    nombre = Column(String(64), index=True, unique=True)
+    nombre = Column(String(255))
 
 class Clase(Base):
     __tablename__ = 'clases'
@@ -41,17 +34,17 @@ class Clase(Base):
 class Actividad(Base):
     __tablename__ = 'actividades'
     id_actividad = Column(Integer, primary_key=True)
-    nombre = Column(String(64), index=True, unique=True)
+    nombre = Column(String(255))
 
 class Aula(Base):
     __tablename__ = 'aulas'
     id_aula = Column(Integer, primary_key=True)
     nombre = Column(String(64), index=True, unique=True)
 
-class Hora(Base):
-    __tablename__ = 'horas'
-    id_hora = Column(Integer, primary_key=True)
-    tramo = Column(String(64), index=True, unique=True)
+class TramoHorario(Base):
+    __tablename__ = 'tramos_horarios'
+    id_tramo_horario = Column(Integer, primary_key=True)
+    nombre = Column(String(64), index=True, unique=True)
     hora_inicio = Column(Time)
     hora_fin = Column(Time)
 
@@ -60,11 +53,11 @@ class Calendario(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     id_profesor = Column(Integer, ForeignKey('profesores.id_profesor'), nullable=False)
     id_profesor_sustituto = Column(Integer, ForeignKey('profesores.id_profesor'))
-    id_actividad = Column(Integer, ForeignKey('actividades.id_actividad'), nullable=False)
-    id_curso = Column(Integer, ForeignKey("cursos.id_curso"), nullable=False)
-    id_clase = Column(Integer, ForeignKey("clases.id_claeses"), nullable=False)
-    id_aula = Column(Integer, ForeignKey('aulas.id_aula'), nullable=False)
+    id_actividad = Column(Integer, ForeignKey('actividades.id_actividad'))
+    id_curso = Column(Integer, ForeignKey("cursos.id_curso"))
+    id_clase = Column(Integer, ForeignKey("clases.id_clase"))
+    id_aula = Column(Integer, ForeignKey('aulas.id_aula'))
     fecha = Column(Date, nullable=False)
-    dia_semana = Column(SQLAlchemyEnum(DiaSemanaEnum))
-    hora = Column(Integer, ForeignKey("horas.id_hora"))
+    dia = Column(Integer, nullable=True)
+    id_tramo_horario = Column(Integer, ForeignKey("tramos_horarios.id_tramo_horario"))
     ausencia = Column(Boolean, default=False)
