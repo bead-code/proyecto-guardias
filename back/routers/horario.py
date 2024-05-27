48ffd46622960d26e13606c5d21abcd108403577
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-05-27 10:25:39 +0200

    Modificar horario adaptar a xml

--- back/routers/horario.py
+++ back/routers/horario.py
@@ -3,6 +3,7 @@ import shutil
 
 from fastapi import APIRouter, UploadFile, File
 from starlette import status
+from starlette.exceptions import HTTPException
 
 router = APIRouter(
     prefix="/horario",
@@ -12,12 +13,16 @@ router = APIRouter(
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.join(BASE_DIR, "..", "generador_horarios", "xml")
 
+
 @router.post("/upload_xml/", status_code=status.HTTP_201_CREATED)
 async def create_upload_file(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+
     try:
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
     except Exception as e:
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=f"Error saving file: {str(e)}")
+
     return {"filename": file.filename, "file_path": file_path}
