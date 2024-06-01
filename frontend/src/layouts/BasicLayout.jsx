1316cb32e26a5a0b42dd1d1121d9e8fe089dd91a
Adolfo Montes Luque <montesluquead@gmail.com>
2024-06-01 16:58:04 +0200

    Modificar layout para el uso de materialUi

--- frontend/src/layouts/BasicLayout.jsx
+++ frontend/src/layouts/BasicLayout.jsx
@@ -1,20 +1,21 @@
-import { Navbar } from "../components/layout/NavBar.jsx";
+import {ModifiedNavBar} from "../components/layout/NavBar.jsx";
 import PropTypes from "prop-types";
-import { Footer } from "../components/layout/Footer.jsx";
+import {Footer} from "../components/layout/Footer.jsx";
 import {ToastContainer} from "react-toastify";
 
-export function BasicLayout({children = ''}) {
+export function BasicLayout({decodedToken, isTokenExpired, setToken, children = ''}) {
+    console.log("decodedToken")
+    console.log(!isTokenExpired)
+
     return (
-    <>
-        <Navbar/>
-        <div className="h-full">
-        {children}
+        <div className="flex flex-col min-h-screen">
+            {!isTokenExpired
+                ? <ModifiedNavBar setToken={setToken}/>
+                : null}
+            <div className="flex-grow w-full h-full place-content-center p-5">
+                {children}
+            </div>
+            <Footer/>
         </div>
-        <ToastContainer/>
-        <Footer/>
-    </>
     )
-}
-BasicLayout.propTypes = {
-    children: PropTypes.node
 }
