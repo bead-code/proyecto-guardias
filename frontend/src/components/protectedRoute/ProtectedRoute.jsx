6eb7f16a5f3f3ad34d67c2c07c8de3a96e0aa8f8
Adolfo Montes Luque <montesluquead@gmail.com>
2024-06-01 17:00:58 +0200

    Eliminar import innecesario

--- frontend/src/components/protectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,5 +1,4 @@
 import {Navigate, Outlet} from "react-router-dom";
-import {Navbar} from "../layout/NavBar.jsx";
 
 export function ProtectedRoute({ children, isAuthenticated, redirectTo = "login", ...props }) {
     if (!isAuthenticated) {
