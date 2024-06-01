8a216ea04793a7867058266a093cdb76a55f2e38
Adolfo Montes Luque <montesluquead@gmail.com>
2024-06-01 16:56:57 +0200

    Modificar tipografia para aceptar className

--- frontend/src/components/tipografia/Tipografia.jsx
+++ frontend/src/components/tipografia/Tipografia.jsx
@@ -1,3 +1,6 @@
-export function Tipografia({children, className = 'text-medium text-left text-black', ...props}) {
+export function Tipografia({children, className, ...props}) {
+    if (className) {
+        return <p className={`text-gray-700 text-sm font-medium ${className}`} {...props}>{children}</p>
+    }
     return <p className={className} {...props}>{children}</p>
 }
