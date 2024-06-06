cdab037de11c7ffcb9031bd9f2ecd7051587885f
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-06 22:43:54 +0200

    Modificar imports

--- back/routers/calendario.py
+++ back/routers/calendario.py
@@ -1,15 +1,16 @@
 import logging
-import os
 from io import BytesIO
+
 
 from fastapi import APIRouter, UploadFile, File, Depends
 from starlette import status
 
 
 from dao import dao_calendario
-from db.database import Session, get_db
+from db.database import Session, get_db, truncate_all_tables
 from db.schemas import CalendarioDTO, ProfesorDTO
-from generador_horarios.conversor_xml_to_df import load_calendario_from_file
+
+from generador_horarios.generador_tablas import generate_tables_from_files
 from security.oauth2 import get_current_profesor
 
 router = APIRouter(
@@ -21,3 +22,6 @@ router = APIRouter(
 def get_calendario_by_id(id: int, current_user: ProfesorDTO = Depends(get_current_profesor), db: Session = Depends(get_db)):
     logging.info(f"Request recibida...")
     return dao_calendario.get_calendario_by_id(id, db)
+
+
+
