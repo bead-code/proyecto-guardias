938da476578f5e058b79096887f9ccb5b51c6033
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-06 22:46:25 +0200

    Añadir endpoint generar_horarios

--- back/routers/calendario.py
+++ back/routers/calendario.py
@@ -23,5 +23,11 @@ def get_calendario_by_id(id: int, current_user: ProfesorDTO = Depends(get_curren
     logging.info(f"Request recibida...")
     return dao_calendario.get_calendario_by_id(id, db)
 
+@router.post("/generar_horarios/", status_code=status.HTTP_200_OK)
+async def upload_tables(tablas: UploadFile = File(...), calenario: UploadFile = File(...), current_user: ProfesorDTO = Depends(get_current_profesor)):
+    tablas_byte = await tablas.read()
+    horarios_byte = await calenario.read()
+    generate_tables_from_files(BytesIO(tablas_byte), BytesIO(horarios_byte))
+    return {"message": "Horarios generados correctamente"}
 
 
