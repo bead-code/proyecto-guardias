91ce6e02526ebb3ddfa47b22afefb4f4c9596566
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-06 22:37:56 +0200

    Anadir endpoint para crear guardia

--- back/routers/guardia.py
+++ back/routers/guardia.py
@@ -30,3 +30,8 @@ async def get_guardias_pendientes(db: Session = Depends(get_db)):
 async def get_guardias_by_profesor(id_profesor: int, db: Session = Depends(get_db), date: Optional[date] = None):
     logging.info(f"Request recibida....")
     return dao_guardia.get_guardias_by_profesor(id_profesor, db, date)
+
+@router.put("{id_profesor}", response_model=List[CalendarioDTO], status_code=status.HTTP_200_OK)
+async def create_guardia(id_profesor: int, fecha_inicio: date, fecha_fin: date, db: Session = Depends(get_db)):
+    logging.info(f"Request recibida....")
+    return dao_guardia.create_guardia(id_profesor, fecha_inicio, fecha_fin, db)
