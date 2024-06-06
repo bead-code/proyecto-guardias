ef17772007112e5099862fdc6f06bf7966334de6
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-06 22:46:45 +0200

    Añadir método crear guardia

--- back/dao/dao_guardia.py
+++ back/dao/dao_guardia.py
@@ -1,8 +1,10 @@
+from datetime import date
 from typing import Optional
 
 from fastapi import HTTPException
 from sqlalchemy import Date
 from starlette import status
+
 from db.database import Session
 from db.models import Calendario
 
@@ -52,3 +54,31 @@ def get_guardias_by_profesor(id: int, db: Session, date: Optional[Date] = None):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No hay guardias asignadas a este profesor")
     return calendario
+
+
+def create_guardia(id_profesor: int, fecha_inicio: date, fecha_fin: date, db: Session):
+    try:
+
+        updated_rows = db.query(Calendario).filter(
+            Calendario.id_profesor == id_profesor,
+            Calendario.fecha >= fecha_inicio,
+            Calendario.fecha <= fecha_fin
+        ).update({"ausencia": True}, synchronize_session='fetch')
+
+        if updated_rows == 0:
+            raise HTTPException(status_code=404, detail="No se encontraron registros para actualizar")
+
+        updated_schedules = db.query(Calendario).filter(
+            Calendario.id_profesor == id_profesor,
+            Calendario.fecha >= fecha_inicio,
+            Calendario.fecha <= fecha_fin,
+            Calendario.ausencia == True
+        ).all()
+
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return updated_schedules
