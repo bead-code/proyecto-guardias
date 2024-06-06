995dacb51cafd33364875c0123c4191cc230549a
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-06 22:38:15 +0200

    Modificar schema devuelto por los endpoint

--- back/routers/grupo_guardia.py
+++ back/routers/grupo_guardia.py
@@ -6,19 +6,19 @@ from starlette import status
 
 from dao import dao_guardia, dao_grupo_guardia
 from db.database import Session, get_db
-from db.schemas import ProfesorDTO
+from db.schemas import ProfesorDTO, CalendarioDTO
 
 router = APIRouter(
     prefix="/grupo_guardia",
     tags=["grupo_guardia"]
 )
 
-@router.get('/', response_model=List[ProfesorDTO], status_code=status.HTTP_200_OK)
+@router.get('/', response_model=List[CalendarioDTO], status_code=status.HTTP_200_OK)
 async def get_guardias_pendientes_by_grupo_guardia(id_tramo: int, dia: int, db: Session = Depends(get_db)):
     logging.info(f"Request recibida...")
     return dao_grupo_guardia.get_guardias_pendientes_by_grupo_guardia(id_tramo, dia, db)
 
-@router.get('/{id_tramo}', response_model=List[ProfesorDTO], status_code=status.HTTP_200_OK)
+@router.get('/{id_tramo}', response_model=List[CalendarioDTO], status_code=status.HTTP_200_OK)
 async def get_guardias_asignadas_by_tramo(id_tramo: int, dia: int, db: Session = Depends(get_db)):
     logging.info(f"Request recibida...")
     return dao_grupo_guardia.get_guardias_pendientes_by_grupo_guardia(id_tramo,dia, db)
