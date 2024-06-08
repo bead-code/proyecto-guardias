4e1cca0db6e5dc5c306e5a558ddd2bf2fea07617
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-08 15:27:28 +0200

    Modificar ordén de los endpoints

--- back/main.py
+++ back/main.py
@@ -15,17 +15,17 @@ from routers import login, profesor, rol, curso, actividad, calendario, clase, a
     grupo_guardia
 
 app = FastAPI()
+app.include_router(login.router)
 app.include_router(profesor.router)
+app.include_router(guardia.router)
+app.include_router(grupo_guardia.router)
+app.include_router(calendario.router)
 app.include_router(rol.router)
 app.include_router(curso.router)
 app.include_router(clase.router)
 app.include_router(actividad.router)
 app.include_router(aula.router)
 app.include_router(tramo_horario.router)
-app.include_router(login.router)
-app.include_router(calendario.router)
-app.include_router(guardia.router)
-app.include_router(grupo_guardia.router)
 origins = [
     "http://localhost",
     "http://localhost:8000",
