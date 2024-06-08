b3a281e33367c5c0dc40273f0d5763b9cf470c68
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-08 23:05:49 +0200

    Modificar nombres de los metodos y utilizar los nuevos métodos de cargar xml

--- back/generador_horarios/generador_tablas.py
+++ back/generador_horarios/generador_tablas.py
@@ -6,11 +6,10 @@ from sqlalchemy import insert
 from sqlalchemy.exc import SQLAlchemyError
 
 from db.database import Session, truncate_all_tables
-from generador_horarios.conversor_xml_to_df import load_tables_from_path, load_tables_from_file, \
-    load_calendario_from_file
+from generador_horarios.conversor_xml_to_df import load_tables, load_calendario
 from generador_horarios.generador_actividades import load_actividades_from_xml
 from generador_horarios.generador_aulas import load_aulas_from_xml
-from generador_horarios.generador_calendario import load_calendario
+from generador_horarios.generador_calendario import generate_calendario
 from generador_horarios.generador_clases import load_clases_from_xml
 from generador_horarios.generador_cursos import load_cursos_from_xml
 from generador_horarios.generador_profesores import load_profesores_from_xml
@@ -18,19 +17,19 @@ from generador_horarios.generador_roles import generar_roles
 from generador_horarios.generador_tramos_horarios import load_tramos_horarios_from_xml
 
 
-def generate_tables_from_path():
+def generate_tables():
     generar_roles()
-    load_profesores_from_xml(load_tables_from_path())
-    load_aulas_from_xml(load_tables_from_path())
-    load_cursos_from_xml(load_tables_from_path())
-    load_actividades_from_xml(load_tables_from_path())
-    load_tramos_horarios_from_xml(load_tables_from_path())
-    load_clases_from_xml(load_tables_from_path())
+    load_profesores_from_xml(load_tables())
+    load_aulas_from_xml(load_tables())
+    load_cursos_from_xml(load_tables())
+    load_actividades_from_xml(load_tables())
+    load_tramos_horarios_from_xml(load_tables())
+    load_clases_from_xml(load_tables())
 
 
 def generate_tables_from_files(tablas: BytesIO, calendario: BytesIO):
-    tablas_df = load_tables_from_file(tablas)
-    calendario_df = load_calendario_from_file(calendario)
+    tablas_df = load_tables(tablas)
+    calendario_df = load_calendario(calendario)
     truncate_all_tables()
     generar_roles()
     load_profesores_from_xml(tablas_df)
@@ -39,4 +38,4 @@ def generate_tables_from_files(tablas: BytesIO, calendario: BytesIO):
     load_actividades_from_xml(tablas_df)
     load_tramos_horarios_from_xml(tablas_df)
     load_clases_from_xml(tablas_df)
-    load_calendario(calendario_df)
+    generate_calendario(calendario_df)
