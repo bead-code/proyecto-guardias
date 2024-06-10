eaa05a86774b65ec64fff895b24ea2b6bde7ba02
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-10 13:03:30 +0200

    Implementar logger en dao_aula

--- back/dao/dao_aula.py
+++ back/dao/dao_aula.py
@@ -1,8 +1,9 @@
-import logging
 from fastapi import HTTPException
 from db.database import Session
 from db.models import Aula
 from db.schemas import AulaCreate, AulaUpdate
+from utils.logger import logger
+
 
 def get_aula_by_id(id: int, db: Session):
     """
@@ -18,7 +19,9 @@ def get_aula_by_id(id: int, db: Session):
     """
     aula = db.query(Aula).filter(Aula.id_aula == id).filter(Aula.activo == True).first()
     if not aula:
+        logger.error(f"El aula con ID {id} no existe en la base de datos")
         raise HTTPException(status_code=404, detail="El aula no existe en la base de datos")
+    logger.info(f"Aula retornada exitosamente")
     return aula
 
 def get_aula_by_nombre(nombre: str, db: Session):
@@ -35,7 +38,9 @@ def get_aula_by_nombre(nombre: str, db: Session):
     """
     aula = db.query(Aula).filter(Aula.nombre == nombre).filter(Aula.activo == True).first()
     if not aula:
+        logger.error(f"El aula con nombre {nombre} no existe en la base de datos")
         raise HTTPException(status_code=404, detail="El aula no existe en la base de datos")
+    logger.info(f"Aula retornada exitosamente")
     return aula
 
 def get_aulas(db: Session):
@@ -50,7 +55,9 @@ def get_aulas(db: Session):
     """
     aulas = db.query(Aula).filter(Aula.activo == True).all()
     if not aulas:
+        logger.error("No existen aulas en la base de datos")
         raise HTTPException(status_code=404, detail="No existe ningún aula en la base de datos")
+    logger.info("Aulas retornadas exitosamente")
     return aulas
 
 def create_aula(request: AulaCreate, db: Session):
@@ -67,6 +74,7 @@ def create_aula(request: AulaCreate, db: Session):
     """
     aula = db.query(Aula).filter(Aula.nombre == request.nombre).filter(Aula.activo == True).first()
     if aula:
+        logger.error(f"El aula con nombre {request.nombre} ya existe en la base de datos")
         raise HTTPException(status_code=409, detail='El aula ya existe en la base de datos')
     new_aula = Aula(
         nombre=request.nombre
@@ -75,9 +83,11 @@ def create_aula(request: AulaCreate, db: Session):
     try:
         db.commit()
         db.refresh(new_aula)
+        logger.info(f"Aula creada exitosamente")
         return new_aula
     except Exception as e:
         db.rollback()
+        logger.error(f"Error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error al insertar el aula en la BBDD: {str(e)}")
 
 def update_aula(id: int, request: AulaUpdate, db: Session):
@@ -99,10 +109,11 @@ def update_aula(id: int, request: AulaUpdate, db: Session):
     try:
         db.commit()
         db.refresh(aula)
+        logger.info(f"Aula actualizada exitosamente")
         return aula
     except Exception as e:
         db.rollback()
-        logging.error(f"Error occurred: {str(e)}")
+        logger.error(f"Error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error al modificar la aula en la base de datos")
 
 def delete_aula(id: int, db: Session):
@@ -120,8 +131,10 @@ def delete_aula(id: int, db: Session):
     try:
         db.commit()
         db.refresh(aula)
+        logger.info(f"Aula eliminada exitosamente")
     except Exception as e:
         db.rollback()
+        logger.error(f"Error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error al borrar el aula de la BBDD: {str(e)}")
 
 
