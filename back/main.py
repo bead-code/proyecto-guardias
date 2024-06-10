300789eb51e5fda2503a64741b216a1eaff4bebe
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-10 13:05:41 +0200

    Modificar par utilizar el nuevo generador de tablas

--- back/main.py
+++ back/main.py
@@ -1,18 +1,8 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
 from db.database import Base, engine, truncate_all_tables
-from generador_horarios.conversor_xml_to_df import load_tables, load_calendario
-from generador_horarios.generador_actividades import load_actividades_from_xml
-from generador_horarios.generador_aulas import load_aulas_from_xml
-from generador_horarios.generador_calendario import generate_calendario
-from generador_horarios.generador_clases import load_clases_from_xml
-from generador_horarios.generador_cursos import load_cursos_from_xml
-from generador_horarios.generador_profesores import load_profesores_from_xml
-from generador_horarios.generador_roles import generar_roles
-from generador_horarios.generador_tramos_horarios import load_tramos_horarios_from_xml
-from routers import login, profesor, rol, curso, actividad, calendario, clase, aula, tramo_horario, guardia, \
-    grupo_guardia
+from generador_calendario.generador_tablas import generate_tables_from_path
+from routers import login, profesor, rol, curso, actividad, calendario, clase, aula, tramo_horario, guardia, grupo_guardia
 
 app = FastAPI()
 app.include_router(login.router)
@@ -44,19 +34,8 @@ app.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-def init_data():
-    generar_roles()
-    load_profesores_from_xml(load_tables())
-    load_aulas_from_xml(load_tables())
-    load_cursos_from_xml(load_tables())
-    load_actividades_from_xml(load_tables())
-    load_tramos_horarios_from_xml(load_tables())
-    load_clases_from_xml(load_tables())
-    generate_calendario(load_calendario())
-
 
 @app.on_event("startup")
 def on_startup():
-    truncate_all_tables()
-    init_data()
+    generate_tables_from_path()
 
