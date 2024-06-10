57e4a636c61e2071bfb551f8fbc8e08def30246b
Adolfo Montes Luque <montesluquead@gmail.com>
2024-06-10 21:34:55 +0200

    Reestructuracion de codigo y validacion de datos frontend

--- frontend/src/components/pages/ausencias/AusenciaUsuario.jsx
+++ frontend/src/components/pages/ausencias/AusenciaUsuario.jsx
@@ -7,10 +7,9 @@ import {
 } from "@material-tailwind/react";
 import {useContext, useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
-import AppGlobal from "../../../App.jsx";
+import {AppGlobal} from "../../../App.jsx";
 import dayjs from "dayjs";
-import {DatePicker} from "@mui/x-date-pickers/DatePicker";
-import {TimePicker} from "@mui/x-date-pickers/TimePicker";
+import {DatePicker, TimePicker} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
 import {mostrarToast} from "../../../utils/Notificaciones.js";
@@ -26,72 +25,79 @@ export function AusenciaUsuario() {
     const [horaFin, setHoraFin] = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:8000/profesor/${idProfesor}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + token,
-            },
-        }).then(async (response) => {
-            if (response.ok) {
-                const data = await response.json();
-                setUsuario(data);
-            } else {
-                console.log("No se ha podido obtener el profesor");
+        const fetchUsuario = async () => {
+            try {
+                const response = await fetch(`http://localhost:8000/profesor/${idProfesor}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + token,
+                    },
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setUsuario(data);
+                } else {
+                    console.log("No se ha podido obtener el profesor");
+                }
+            } catch (error) {
+                console.error("Error:", error);
+                mostrarToast("No se ha podido obtener el profesor", "error");
             }
-        });
+        };
+        fetchUsuario();
     }, [idProfesor, token]);
 
-    const handleCrearAusencia = () => {
-        const startDateTime = dayjs(`${fechaInicio}T${horaInicio}`);
-        const endDateTime = dayjs(`${fechaFin}T${horaFin}`);
-        console.log("(dayjs(fechaInicio).isSame(dayjs(fechaFin)) && dayjs(horaInicio).isAfter(dayjs(horaFin)))")
-        console.log(startDateTime)
-        console.log(endDateTime)
-        if (!fechaInicio || !fechaFin || !horaInicio || !horaFin) {
-            mostrarToast("Rellena todos los campos", "error");
-            return;
-        }
+    const handleCrearAusencia = async () => {
+        try {
+            const startDateTime = dayjs(`${fechaInicio}T${horaInicio}`);
+            const endDateTime = dayjs(`${fechaFin}T${horaFin}`);
 
-        if (startDateTime.isAfter(endDateTime)) {
-            mostrarToast("La fecha de inicio no puede ser mayor a la fecha de fin", "error");
-            return;
-        }
+            if (!fechaInicio || !fechaFin || !horaInicio || !horaFin) {
+                mostrarToast("Rellena todos los campos", "error");
+                return;
+            }
 
-        const queryParams = new URLSearchParams({
-            id_profesor: idProfesor,
-            fecha_inicio: fechaInicio,
-            fecha_fin: fechaFin,
-            hora_inicio: horaInicio,
-            hora_fin: horaFin,
-        }).toString();
+            if (startDateTime.isAfter(endDateTime)) {
+                mostrarToast("La fecha de inicio no puede ser mayor a la fecha de fin", "error");
+                return;
+            }
 
-        fetch(`http://localhost:8000/guardias?${queryParams}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + token,
-            },
-        }).then(async (response) => {
+            const queryParams = new URLSearchParams({
+                id_profesor: idProfesor,
+                fecha_inicio: fechaInicio,
+                fecha_fin: fechaFin,
+                hora_inicio: horaInicio,
+                hora_fin: horaFin,
+            }).toString();
+
+            const response = await fetch(`http://localhost:8000/guardias?${queryParams}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                },
+            });
+
             if (response.ok) {
                 navigate("/guardias");
             } else {
                 mostrarToast("No se ha podido crear la ausencia", "error");
             }
-        }).catch((error) => {
+        } catch (error) {
             console.error("Error:", error);
             mostrarToast("No se ha podido crear la ausencia", "error");
-        });
+        }
     };
 
-    const handleCabioFecha = (date, setDate) => {
+    const handleCambiarFecha = (date, setDate) => {
         if (date) {
             const formattedDate = date.format("YYYY-MM-DD");
             setDate(formattedDate);
         }
     };
 
-    const handleCabioHora = (hour, setHour) => {
+    const handleCambiarHora = (hour, setHour) => {
         if (hour) {
             const formattedHour = hour.format("HH:mm:ss");
             setHour(formattedHour);
@@ -117,7 +123,7 @@ export function AusenciaUsuario() {
                             Usuario:
                         </Typography>
                         <Typography variant="lead" color="black" size="xl" className="mr-auto">
-                            {usuario?.nombre ? usuario.nombre : "No se ha podido obtener el usuario"}
+                            {usuario?.nombre || "No se ha podido obtener el usuario"}
                         </Typography>
                     </div>
                     <div className="flex flex-wrap flex-row gap-2">
@@ -128,14 +134,14 @@ export function AusenciaUsuario() {
                             <DatePicker
                                 disablePast
                                 views={["year", "month", "day"]}
-                                onChange={(date) => handleCabioFecha(date, setFechaInicio)}
+                                onChange={(date) => handleCambiarFecha(date, setFechaInicio)}
                                 minDate={minDate}
                                 maxDate={maxDate}
                                 className="sm:w-full md:w-1/3 lg:w-1/4"
                             />
                             <TimePicker
                                 views={["hours", "minutes"]}
-                                onChange={(hour) => handleCabioHora(hour, setHoraInicio)}
+                                onChange={(hour) => handleCambiarHora(hour, setHoraInicio)}
                                 className="sm:w-full md:w-1/3 lg:w-1/4"
                                 minTime={minHour}
                                 maxTime={maxHour}
@@ -150,14 +156,14 @@ export function AusenciaUsuario() {
                             <DatePicker
                                 disablePast
                                 views={["year", "month", "day"]}
-                                onChange={(date) => handleCabioFecha(date, setFechaFin)}
+                                onChange={(date) => handleCambiarFecha(date, setFechaFin)}
                                 minDate={minDate}
                                 maxDate={maxDate}
                                 className="sm:w-full md:w-1/3 lg:w-1/4"
                             />
                             <TimePicker
                                 views={["hours", "minutes"]}
-                                onChange={(hour) => handleCabioHora(hour, setHoraFin)}
+                                onChange={(hour) => handleCambiarHora(hour, setHoraFin)}
                                 className="sm:w-full md:w-1/3 lg:w-1/4"
                                 minTime={minHour}
                                 maxTime={maxHour}
