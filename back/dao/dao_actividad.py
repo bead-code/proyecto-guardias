45c2023995591aeb38e3ace5aeeba1133965f8e6
Enrique Cillero Dorado <quique.cillerodorado@gmail.com>
2024-06-10 13:05:07 +0200

    Implementar logger en dao_actividad

--- back/dao/dao_actividad.py
+++ back/dao/dao_actividad.py
@@ -1,8 +1,9 @@
-import logging
 from fastapi import HTTPException
 from db.database import Session
 from db.models import Actividad
 from db.schemas import ActividadCreate, ActividadUpdate
+from utils.logger import logger
+
 
 def get_actividad_by_id(id: int, db: Session):
     """
@@ -18,7 +19,9 @@ def get_actividad_by_id(id: int, db: Session):
     """
     actividad = db.query(Actividad).filter(Actividad.id_actividad == id).filter(Actividad.activo == True).first()
     if not actividad:
+        logger.error(f"La actividad con ID {id} no existe en la base de datos")
         raise HTTPException(status_code=404, detail="La actividad no existe en la base de datos")
+    logger.info(f"Actividad retornada exitosamente")
     return actividad
 
 def get_actividad_by_nombre(nombre: str, db: Session):
@@ -35,7 +38,9 @@ def get_actividad_by_nombre(nombre: str, db: Session):
     """
     actividad = db.query(Actividad).filter(Actividad.nombre == nombre).filter(Actividad.activo == True).first()
     if not actividad:
+        logger.error(f"La actividad con nombre {nombre} no existe en la base de datos")
         raise HTTPException(status_code=404, detail="La actividad no existe en la base de datos")
+    logger.info(f"Actividad retornada exitosamente")
     return actividad
 
 def get_actividades(db: Session):
@@ -50,7 +55,9 @@ def get_actividades(db: Session):
     """
     actividades = db.query(Actividad).filter(Actividad.activo == True).all()
     if not actividades:
+        logger.error("No hay actividades registradas en la base de datos")
         raise HTTPException(status_code=404, detail="No hay actividades registradas en la base de datos")
+    logger.info("Actividades retornadas exitosamente")
     return actividades
 
 def create_actividad(request: ActividadCreate, db: Session):
@@ -67,19 +74,20 @@ def create_actividad(request: ActividadCreate, db: Session):
     """
     actividad = db.query(Actividad).filter(Actividad.nombre == request.nombre).first()
     if actividad:
+        logger.error(f"La actividad con nombre {request.nombre} ya existe en la base de datos")
         raise HTTPException(status_code=409, detail='La actividad ya existe en la base de datos')
     new_actividad = Actividad(
         nombre=request.nombre,
     )
     db.add(new_actividad)
     try:
-        logging.info("Insertando la actividad en la base de datos...")
         db.commit()
         db.refresh(new_actividad)
+        logger.info("Actividad creada exitosamente")
         return new_actividad
     except Exception as e:
         db.rollback()
-        logging.error(f"Error occurred: {str(e)}")
+        logger.error(f"Error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error al insertar la actividad en la BBDD: {str(e)}")
 
 def update_actividad(id: int, request: ActividadUpdate, db: Session):
@@ -101,10 +109,11 @@ def update_actividad(id: int, request: ActividadUpdate, db: Session):
     try:
         db.commit()
         db.refresh(actividad)
+        logger.info("Actividad actualizada exitosamente")
         return actividad
     except Exception as e:
         db.rollback()
-        logging.error(f"Error occurred: {str(e)}")
+        logger.error(f"Error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error al modificar la actividad en la base de datos")
 
 def delete_actividad(id: int, db: Session):
@@ -122,8 +131,10 @@ def delete_actividad(id: int, db: Session):
     try:
         db.commit()
         db.refresh(actividad)
+        logger.info("Actividad eliminada exitosamente")
     except Exception as e:
         db.rollback()
+        logger.error(f"Error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error borrando la actividad de la base de datos: {str(e)}")
 
 
